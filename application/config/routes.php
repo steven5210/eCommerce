878d26e670715df94af5a5e14e5a9c3bf5f0a821
Steven5210 <shuynh5210@msn.com>
2015-08-31 15:42:12 -0700

    Route and Controller
    Updated Route and Controller

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,6 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 
-$route['default_controller'] = "welcome";
+$route['default_controller'] = "items";
 $route['404_override'] = '';
 
