c62182aee9770ae7ffd13fac9dc61e50212fd19d
Steven5210 <shuynh5210@msn.com>
2015-08-31 14:28:29 -0700

    Default Start

---
+++ application/controllers/welcome.php
@@ -0,0 +1,18 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	public function __construct()
+       {
+            parent::__construct();
+            // Your own constructor code
+       }
+	
+	public function index()
+	{
+		$this->load->view('welcome_message');
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
---
+++ application/views/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Place searches</title>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+    <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/css/materialize.min.css">
+
+  <!-- Compiled and minified JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/js/materialize.min.js"></script>
+   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <script type="text/javascript">
+    $(document).ready(function() {
+      $('select').material_select();
+  });
+   </script>
+   </head>
+<body class='container'>
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">PlaceHolder eCommerce</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="collapsible.html">Shopping Cart(40)</a></li>
+      </ul>
+    </div>
+  </nav>
+  <div id='side_nav'>
+  </div>
+
+</body>
+</html>
