8ecf40cfc2b953db02857e0affb77947f3c03588
Preston <Plphan206@hotmail.com>
2015-09-01 11:15:50 -0700

    added index page

--- application/views/index.php
+++ application/views/index.php
@@ -17,6 +17,54 @@
       $('select').material_select();
   });
    </script>
+   <style>
+    #side_nav {
+      width: 200px;
+      border: 1px solid black;
+      display: inline-block;
+      vertical-align: top;
+    }
+    #main_content {
+      display: inline-block;
+      border: 1px solid black;
+      display: inline-block;
+      width: 75.5%;
+
+    }
+    #main_content h4, ul {
+      display: inline-block;
+      vertical-align: top;
+    }
+    #main_content ul {
+      margin-left: 45%;
+    }
+    #item_nav li {
+      display: inline-block;
+    }
+    #wrapper {
+      margin-top: 15px;
+    }
+    #item_nav li:not(:last-child) {
+      border-right: 1px solid black;
+      padding-right: 5px;
+    }
+    td {
+      border: 1px solid black;
+      height: 100px;
+      width: 100px;
+    }
+    table {
+      height: 700px;
+    }
+    #pagination li {
+      display: inline-block;
+    }
+    #pagination li:not(:last-child){
+      border-right: 1px solid black;
+      padding-right: 5px;
+    }
+
+   </style>
    </head>
 <body class='container'>
   <nav>
@@ -27,8 +75,70 @@
       </ul>
     </div>
   </nav>
-  <div id='side_nav'>
+  <div id="wrapper">
+    <div id='side_nav'>
+      <form action="search_by_name" method="post">
+        <input type="text" name="search" placeholder="Product name">
+        <input type="submit" value="search">
+      </form>
+      <h5>Categories</h5>
+      <ul>
+        <li><a href="#">temporary category list item</a></li>
+        <li><a href="#">temporary category list item</a></li>
+        <li><a href="#">temporary category list item</a></li>
+        <li><a href="#">temporary category list item</a></li>
+        <li><a href="#">temporary category list item</a></li>
+      </ul>
+    </div>
+    <div id="main_content">
+      <h4>Tshirts (page 2)</h4>
+      <ul id="item_nav">
+        <li><a href="#">first</a></li>
+        <li><a href="#">prev</a></li>
+        <li><a href="#">2</a></li>
+        <li><a href="#">next</a></li>
+      </ul>
+      <form action="sort_by" method="post">
+        <p>Sorted by
+          <select name="sort">
+            <option value="#">-Select-</option>
+            <option value="price_lowest">Price lowest</option>
+            <option value="price_highest">Price highest</option>
+            <option value="most_popular">Most Popular</option>
+          </select>
+        </p>
+      </form>
+      <div id="items_list">
+        <table>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </table>
+      </div>
+      <ul id="pagination">
+        <li><a href="#">1</a></li>
+        <li><a href="#">2</a></li>
+        <li><a href="#">3</a></li>
+      </ul>
+    </div>
   </div>
-  <a href="/product_info">Product Info Page</a>
 </body>
 </html>
