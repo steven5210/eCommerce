45a138070c330348ecb96712fc2d485393a78687
Preston <Plphan206@hotmail.com>
2015-09-02 14:06:11 -0700

    troube loading local host

--- application/controllers/items.php
+++ application/controllers/items.php
@@ -3,7 +3,9 @@ class items extends CI_Controller {
  
 	public function __construct()
        {
+       		
             parent::__construct();
+
             // Your own constructor code
        }
 	public function index()
